6eaf14cf6c7772e7859b435ce5c682d86850ccad
KIM HYOUNG MIN <rlagud2005@naver.com>
2024-03-25 15:38:12 +0900

    초기 커밋.

---
+++ HKCBusbarInspection/Schemas/환경설정.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HKCBusbarInspection.Schemas
+{
+    public class 환경설정
+    {
+
+    }
+}
---
+++ HKCBusbarInspection/UI/Form/MainForm.cs
@@ -0,0 +1,28 @@
+﻿using DevExpress.XtraBars;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace HKCBusbarInspection
+{
+    public partial class MainForm : DevExpress.XtraBars.TabForm
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+        void OnOuterFormCreating(object sender, OuterFormCreatingEventArgs e)
+        {
+            MainForm form = new MainForm();
+            form.TabFormControl.Pages.Clear();
+            e.Form = form;
+            OpenFormCount++;
+        }
+        static int OpenFormCount = 1;
+    }
+}
